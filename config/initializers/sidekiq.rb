21ad6e25640ca1af17dc55cc99acd065c11d239c
Dave Paola <dpaola2@gmail.com>
2021-02-20 14:11:06 -0800

    sidekiq config for heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,2 +1,10 @@
+Sidekiq.configure_server do |config|
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
+
 require "sidekiq/throttled"
 Sidekiq::Throttled.setup!
